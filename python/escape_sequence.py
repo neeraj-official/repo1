d9df77eb17a34efba3d3f2967706bd7f96e89a3c
Neeraj Kumar <neeraj.nidu@gmail.com>
2022-04-05 11:17:41 +0530

    neeraj python files

---
+++ python/escape_sequence.py
@@ -0,0 +1,16 @@
+# \' single quote
+# \"  double quote
+# \\ - \
+# \n  - newline
+# \t - tab
+# \b backspace
+
+print(" hello \"world \" ")
+#newline
+print("line A \nline B \nline C")
+#tab
+print("lineA\tNeeraj")
+#wanna print backslash
+print("this is \ backslash")
+#backslash in the end
+print("this is backslash\\")
---
+++ python/print_func.py
@@ -0,0 +1,9 @@
+#collection of character in single or double quote
+
+print("Hello world")
+print('hello world')
+
+print("Hello  'Hi' world")
+print('hello  "Hi" world')
+
+print(" Hello I'm Neeraj")
